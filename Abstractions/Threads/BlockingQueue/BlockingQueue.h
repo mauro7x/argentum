73bb0fd91629c77c00a6a4124132d44b9583d011
Mauro7x <mparafati@fi.uba.ar>
2020-05-25 19:20:56 -0300

    Blocking queue de instancias

---
+++ Abstractions/Threads/BlockingQueue/BlockingQueue.h
@@ -0,0 +1,100 @@
+#ifndef __BLOCKING_QUEUE_H__
+#define __BLOCKING_QUEUE_H__
+
+//-----------------------------------------------------------------------------
+#include <condition_variable>
+#include <exception>
+#include <mutex>
+#include <queue>
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+
+template <class T>
+class BlockingQueue {
+   private:
+    std::mutex m;
+    std::queue<T> queue;
+    std::condition_variable cv;
+    bool permanently_closed;
+
+   public:
+    /**
+     * Descripción: constructor. Inicia el closed en false.
+     *
+     * Parámetros: -
+     */
+    BlockingQueue() : permanently_closed(false) {}
+
+    /* Deshabilitamos el constructor por copia. */
+    BlockingQueue(const BlockingQueue&) = delete;
+
+    /* Deshabilitamos el operador= para copia.*/
+    BlockingQueue& operator=(const BlockingQueue&) = delete;
+
+    /* Deshabilitamos el constructor por movimiento. */
+    BlockingQueue(BlockingQueue&& other) = delete;
+
+    /* Deshabilitamos el operador= para movimiento. */
+    BlockingQueue& operator=(BlockingQueue&& other) = delete;
+
+    /**
+     * Descripción: inserta un objeto a la cola, y notifica a todos
+     * los threads que esperan por la condition-variable.
+     *
+     * Parámetros: objeto de clase T.
+     *
+     * Retorno: -
+     */
+    void push(T t) {
+        std::unique_lock<std::mutex> l(m);
+        queue.push(t);
+        cv.notify_all();
+    }
+
+    /**
+     * Descripción: extrae un objeto de la cola. Si la misma está vacía,
+     * pone a dormir al thread solicitante. Si la misma está vacía y
+     * además cerrada, lanza excepción.
+     *
+     * Parámetros: -
+     *
+     * Retorno: objeto de clase T.
+     */
+    T pop() {
+        std::unique_lock<std::mutex> l(m);
+        while (queue.empty()) {
+            if (permanently_closed) {
+                throw std::exception;
+            }
+
+            cv.wait(l);
+        }
+
+        T t = queue.front();
+        queue.pop();
+        return t;
+    }
+
+    /**
+     * Descripción: cierra la cola definitivamente, notificando a todos
+     * los threads que esperan por la condition-variable.
+     *
+     * Parámetros: -
+     *
+     * Retorno: -
+     */
+    void close() {
+        std::unique_lock<std::mutex> l(m);
+        permanently_closed = true;
+        cv.notify_all();
+    }
+
+    /**
+     * Descripción: destructor.
+     */
+    ~BlockingQueue() {}
+};
+
+//-----------------------------------------------------------------------------
+#endif  // __BLOCKING_QUEUE_H__
---
+++ Abstractions/Threads/BlockingQueue/README.md
@@ -0,0 +1,10 @@
+# BlockingQueue
+
+Implementación de una cola bloqueante para instancias. En caso de estar vacía y cerrada, el método pop lanzará una excepción.
+
+## Librerías necesarias
+
+Se utilizan las siguientes librerías necesarias:
+
+* (Exception)
+    * ClosedQueueException
