d884bee3463f87911dfd1b872467d3b782820fc4
Mauro7x <mparafati@fi.uba.ar>
2020-06-03 20:59:34 -0300

    idea basica de ciclo

--- Client/src/Model/Client.cpp
+++ Client/src/Model/Client.cpp
@@ -11,13 +11,32 @@
 Client::Client() {}
 
 void Client::run() {
-    std::cout << "Comienza la ejecución del cliente." << std::endl;
+    fprintf(stderr, "DEBUG: Comienza la ejecución del cliente\n");
 
-    // Ejecución del cliente
+    /**
+     * La idea es la siguiente: vamos a presentar 3 pantallas independientes al
+     * cliente en el siguiente orden:
+     *
+     * 1. ServerConnectionView: se pedira una ip y un puerto para conectarse.
+     * Cuando se logre la conexión, se retornará el socket conectado y se
+     * cerrará la ventana.
+     *
+     * 2. LogInView: con el socket conectado, esta pantalla buscará loggear al
+     * jugador con usuario y contraseña, y le dará la opcion de crear un nuevo
+     * jugador. Selección de personaje.
+     *
+     * 3. GameView: una vez que se seleccionó el personaje, se inicia el juego.
+     */
+
+    /* 1. ServerConnectionView */
+
+    /* 2. LogInView */
+
+    /* 3. Game view */
     GameView view;
     view();
 
-    std::cout << "Termina la ejecución del cliente." << std::endl;
+    fprintf(stderr, "DEBUG: Termina la ejecución del cliente\n");
 }
 
 Client::~Client() {}
