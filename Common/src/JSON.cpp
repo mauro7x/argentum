305478f5143824288a309ce9959a5c5e66ddd032
Mauro7x <mparafati@fi.uba.ar>
2020-06-09 20:10:48 -0300

    Encapsulo lectura de jsons

---
+++ Common/src/JSON.cpp
@@ -0,0 +1,25 @@
+#include "../includes/JSON.h"
+
+//-----------------------------------------------------------------------------
+
+json _loadJsonFile(const std::string& filepath) {
+    std::ifstream file(filepath);
+    if (file.fail()) {
+        throw Exception("Error opening file: %s", filepath);
+    }
+
+    json j;
+    file >> j;
+    if (file.fail()) {
+        throw Exception("Error reading file: %s", filepath);
+    }
+
+    file.close();
+    if (file.fail()) {
+        throw Exception("Error closing file: %s", filepath);
+    }
+
+    return j;
+}
+
+//-----------------------------------------------------------------------------
