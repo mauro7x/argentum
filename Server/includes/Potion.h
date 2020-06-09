524419da3c4757a6771f5ef961200c66d12928fd
sankle <santi.klein@hotmail.com>
2020-06-09 14:58:45 -0300

    Implemento PotionFactory

--- Server/includes/Potion.h
+++ Server/includes/Potion.h
@@ -2,8 +2,14 @@
 #define __POTION_H__
 
 #include <string>
+#include <exception>
 
 #include "Item.h"
+
+class UnknownPotionTypeException: public std::exception {
+    public:
+        virtual const char* what() const noexcept;
+};
 
 class Potion: public Item {
     protected:
@@ -21,29 +27,28 @@ class Potion: public Item {
         Potion(Potion&&) = delete;
         Potion& operator=(Potion&&) = delete;
 
-        virtual void equip(Character& character) = 0;
+        virtual void equip(Character& equipper) = 0;
+};
+
+class PotionFactory {
+    public:
+        static Potion* newPotion(PotionCfg data);
 };
 
 class HealthPotion: public Potion {
     public:
-        HealthPotion(const unsigned int id, 
-                     const std::string name,
-                     const unsigned int price,
-                     const unsigned int recovery_points);
+        HealthPotion(PotionCfg data);
         ~HealthPotion();
 
-        virtual void equip(Character& character) override;
+        virtual void equip(Character& equipper) override;
 };
 
 class ManaPotion: public Potion {
     public:
-        ManaPotion(const unsigned int id,
-                   const std::string name,
-                   const unsigned int price,
-                   const unsigned int recovery_points);
+        ManaPotion(PotionCfg data);
         ~ManaPotion();
 
-        virtual void equip(Character& character) override;
+        virtual void equip(Character& equipper) override;
 };
 
 #endif
