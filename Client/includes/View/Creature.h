5e05caa97767c55500ed6ef81564583d1838089e
Mauro7x <mparafati@fi.uba.ar>
2020-06-12 04:23:45 -0300

    agrego Creature

---
+++ Client/includes/View/Creature.h
@@ -0,0 +1,57 @@
+#ifndef __CREATURE_H__
+#define __CREATURE_H__
+
+//-----------------------------------------------------------------------------
+#include <cstdint>
+
+#include "../../../Common/includes/Exceptions/Exception.h"
+#include "Unit.h"
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+
+class Creature : public Unit {
+   private:
+    // Cuerpo
+    Id creature_id;
+
+    /* Copia la data desde el paquete recibido */
+    void _copyData(const CreatureData& init_data);
+
+   public:
+    /* Constructor */
+    Creature(Renderer* renderer, UnitSpriteContainer* sprites, const int tile_w,
+             const int tile_h, const int tile_movement_time);
+
+    /* Deshabilitamos el constructor por copia. */
+    Creature(const Creature&) = delete;
+
+    /* Deshabilitamos el operador= para copia.*/
+    Creature& operator=(const Creature&) = delete;
+
+    /* Habilitamos el constructor por movimiento. */
+    Creature(Creature&& other);
+
+    /* Habilitamos el operador= para movimiento. */
+    Creature& operator=(Creature&& other);
+
+    //-------------------------------------------------------------------------
+
+    /* Se inicializa con el primer paquete del server */
+    void init(const CreatureData& init_data);
+
+    /* Actualizar información según lo que diga el servidor */
+    void update(const CreatureData& updated_data);
+
+    /* Renderizarse si se encuentra dentro de la cámara */
+    void render() const override;
+
+    //-------------------------------------------------------------------------
+
+    /* Destructor */
+    ~Creature();
+};
+
+//-----------------------------------------------------------------------------
+
+#endif  // __CREATURE_H__
