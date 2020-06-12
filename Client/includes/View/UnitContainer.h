48a736650dd4be1f2572c8705919dd19cd820160
Mauro7x <mparafati@fi.uba.ar>
2020-06-12 05:54:58 -0300

    agrego template para contenedor de unidades que utiliza hashmap

---
+++ Client/includes/View/UnitContainer.h
@@ -0,0 +1,102 @@
+#ifndef __UNIT_CONTAINER_H__
+#define __UNIT_CONTAINER_H__
+
+//-----------------------------------------------------------------------------
+//#include <cstdint>
+#include <fstream>
+#include <unordered_map>
+
+#include "../../../Common/includes/Exceptions/Exception.h"
+#include "../../../Common/includes/JSON.h"
+#include "../../../Common/includes/UnitData.h"
+#include "Character.h"
+#include "Creature.h"
+#include "Renderer.h"
+#include "Unit.h"
+#include "UnitSpriteContainer.h"
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+
+template <class ConcreteUnit, class Data>
+class UnitContainer {
+   private:
+    Renderer* g_renderer;
+    UnitSpriteContainer* g_sprites;
+    std::unordered_map<Id, ConcreteUnit> content;
+    int tile_w, tile_h;
+    float tile_movement_time;
+
+   public:
+    /* Constructor */
+    UnitContainer(Renderer* renderer, UnitSpriteContainer* sprites)
+        : g_renderer(renderer), g_sprites(sprites) {}
+
+    /* Deshabilitamos el constructor por copia. */
+    UnitContainer(const UnitContainer&) = delete;
+
+    /* Deshabilitamos el operador= para copia.*/
+    UnitContainer& operator=(const UnitContainer&) = delete;
+
+    /* Deshabilitamos el constructor por movimiento. */
+    UnitContainer(UnitContainer&& other) = delete;
+
+    /* Deshabilitamos el operador= para movimiento. */
+    UnitContainer& operator=(UnitContainer&& other) = delete;
+
+    //-------------------------------------------------------------------------
+
+    /* Inicializa recursos */
+    void init(const int tile_w, const int tile_h,
+              const float tile_movement_time) {
+        this->tile_w = tile_w;
+        this->tile_h = tile_h;
+        this->tile_movement_time = tile_movement_time;
+    }
+
+    /* Agrega una nueva unidad con el id y la data recibida. */
+    void add(const Id id, const Data& init_data) {
+        if (content.count(id) > 0) {
+            throw Exception("ID already taken by another unit (id repeated).");
+        }
+
+        ConcreteUnit new_unit(g_renderer, g_sprites, tile_w, tile_h,
+                              tile_movement_time);
+        new_unit.init(init_data);
+        content.emplace(id, std::move(new_unit));
+    }
+
+    /* Actualiza la unidad con la data recibida. */
+    /* Si el id es inválido lanza una excepción informándolo. */
+    void update(const Id id, const Data& updated_data) {
+        if (content.count(id) == 0) {
+            throw Exception("Attempt to update unknown unit (id is invalid).");
+        }
+
+        content.at(id).update(updated_data);
+    }
+
+    /* Elimina la unidad. */
+    /* Si el id es inválido lanza una excepción informándolo. */
+    void remove(const Id id) {
+        if (content.count(id) == 0) {
+            throw Exception("Attempt to remove unknown unit (id is invalid).");
+        }
+
+        content.erase(id);
+    }
+
+    /* Elimina todas las unidades. */
+    void clear() {
+        content.clear();
+    }
+
+    //-------------------------------------------------------------------------
+
+    /* Destructor */
+    ~UnitContainer() {}
+};
+
+//-----------------------------------------------------------------------------
+
+#endif  // __UNIT_CONTAINER_H__
