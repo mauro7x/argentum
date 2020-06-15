8bc973252aa8b6b0abca3d6efd2e7c9015567f98
Mauro7x <mparafati@fi.uba.ar>
2020-06-14 23:39:31 -0300

    EventHandler refactor,se gana claridad con enums y se permite el mappeo de las teclas a utilizar

--- Client/includes/View/EventHandler.h
+++ Client/includes/View/EventHandler.h
@@ -3,31 +3,69 @@
 
 //-----------------------------------------------------------------------------
 #include <SDL2/SDL.h>
+#include <stdio.h>
 //-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
 #include <atomic>
+#include <unordered_map>
 //-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
 //#include "../../../Common/includes/Exceptions/Exception.h"
 #include "../../../Common/includes/Queue.h"
 #include "../../../Common/includes/Thread.h"
-#include "Window.h"
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+
+enum Event {
+    /* Evento que no nos interesa */
+    INVALID,
+
+    /* Movimiento */
+    START_MOVING_UP,
+    START_MOVING_DOWN,
+    START_MOVING_LEFT,
+    START_MOVING_RIGHT,
+    STOP_MOVING,
+
+    /* Cierre del juego */
+    EXIT
+};
+
+enum Key { UNMAPPED_KEY, UP_KEY, DOWN_KEY, LEFT_KEY, RIGHT_KEY, ESC_KEY };
+
 //-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
 
 class EventHandler : public Thread {
    private:
-    Window& g_window;
     std::atomic_bool& view_running;
     Queue<int*>& requests;
+    std::unordered_map<SDL_Keycode, Key> keys;
+
+    /* Flags internos */
+    Key key_pressed;
+
+    /* Bindea las keycodes de entrada */
+    void _bindKeycodes();
+
+    /* Traduce el Keycode de SDL a una key esperada */
+    Key _getKey(const SDL_Keycode& key);
+
+    /* Traduce el evento del sistema en un evento esperado */
+    Event _getEvent(const SDL_Event& e);
+
+    //-------------------------------------------------------------------------
+    // MANEJO DE EVENTOS PARTICULARES
+
+    //-------------------------------------------------------------------------
 
    public:
     /* Constructor */
-    EventHandler(Window& window, std::atomic_bool& view_running,
-                 Queue<int*>& requests);
+    EventHandler(std::atomic_bool& view_running, Queue<int*>& requests);
 
     /* Método de ejecución del hilo */
     void run() override;
--- Client/src/View/EventHandler.cpp
+++ Client/src/View/EventHandler.cpp
@@ -3,78 +3,220 @@
 //-----------------------------------------------------------------------------
 // Métodos privados
 
+void EventHandler::_bindKeycodes() {
+    keys.emplace(SDLK_w, UP_KEY);
+    keys.emplace(SDLK_s, DOWN_KEY);
+    keys.emplace(SDLK_a, LEFT_KEY);
+    keys.emplace(SDLK_d, RIGHT_KEY);
+    keys.emplace(SDLK_ESCAPE, ESC_KEY);
+}
+
+Key EventHandler::_getKey(const SDL_Keycode& key) {
+    if (keys.count(key)) {
+        return keys.at(key);
+    }
+
+    return UNMAPPED_KEY;
+}
+
+Event EventHandler::_getEvent(const SDL_Event& e) {
+    Key key;
+
+    if (e.type == SDL_QUIT) {
+        return EXIT;
+    }
+
+    if (e.type == SDL_KEYDOWN /*&& e.key.repeat == 0*/) {
+        key = _getKey(e.key.keysym.sym);
+
+        switch (key) {
+            case UNMAPPED_KEY: {
+                break;
+            }
+
+            case UP_KEY: {
+                if (!key_pressed) {
+                    key_pressed = UP_KEY;
+                    return START_MOVING_UP;
+                }
+                break;
+            }
+
+            case DOWN_KEY: {
+                if (!key_pressed) {
+                    key_pressed = DOWN_KEY;
+                    return START_MOVING_DOWN;
+                }
+                break;
+            }
+
+            case LEFT_KEY: {
+                if (!key_pressed) {
+                    key_pressed = LEFT_KEY;
+                    return START_MOVING_LEFT;
+                }
+                break;
+            }
+
+            case RIGHT_KEY: {
+                if (!key_pressed) {
+                    key_pressed = RIGHT_KEY;
+                    return START_MOVING_RIGHT;
+                }
+                break;
+            }
+
+            case ESC_KEY: {
+                return EXIT;
+            }
+
+            default: {
+                break;
+            }
+        }
+    }
+
+    if (e.type == SDL_KEYUP && e.key.repeat == 0) {
+        key = _getKey(e.key.keysym.sym);
+
+        switch (key) {
+            case UNMAPPED_KEY: {
+                break;
+            }
+
+            case UP_KEY: {
+                if (key_pressed == UP_KEY) {
+                    key_pressed = UNMAPPED_KEY;
+                    return STOP_MOVING;
+                }
+                break;
+            }
+
+            case DOWN_KEY: {
+                if (key_pressed == DOWN_KEY) {
+                    key_pressed = UNMAPPED_KEY;
+                    return STOP_MOVING;
+                }
+                break;
+            }
+
+            case LEFT_KEY: {
+                if (key_pressed == LEFT_KEY) {
+                    key_pressed = UNMAPPED_KEY;
+                    return STOP_MOVING;
+                }
+                break;
+            }
+
+            case RIGHT_KEY: {
+                if (key_pressed == RIGHT_KEY) {
+                    key_pressed = UNMAPPED_KEY;
+                    return STOP_MOVING;
+                }
+                break;
+            }
+
+            default: {
+                break;
+            }
+        }
+    }
+
+    return INVALID;
+}
+
 //-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
 // API Pública
 
-EventHandler::EventHandler(Window& window, std::atomic_bool& view_running,
+EventHandler::EventHandler(std::atomic_bool& view_running,
                            Queue<int*>& requests)
-    : g_window(window), view_running(view_running), requests(requests) {}
+    : view_running(view_running),
+      requests(requests),
+      key_pressed(UNMAPPED_KEY) {
+    _bindKeycodes();
+}
 
 void EventHandler::run() {
     SDL_Event e;
+    Event event;
+
+    /* Proxy */
     int* cmd = NULL;
 
     while (view_running) {
         while (SDL_PollEvent(&e) != 0) {
-            /* El cliente cerró el juego */
-            if ((e.type == SDL_QUIT) ||
-                (e.type == SDL_KEYDOWN && e.key.repeat == 0 &&
-                 e.key.keysym.sym == SDLK_ESCAPE)) {
-                view_running = false;
-                break;
-            }
+            event = _getEvent(e);
 
-            if (e.type == SDL_KEYDOWN && e.key.repeat == 0) {
-                switch (e.key.keysym.sym) {
-                    case SDLK_ESCAPE:
-                        view_running = false;
-                        break;
-                    case SDLK_w:
-                        cmd = new int(0);
-                        fprintf(stderr, "Soy el cliente. Enviamos un %i.\n",
-                                *cmd);
-                        requests.push(cmd);
-                        break;
-
-                    case SDLK_s:
-                        cmd = new int(1);
-                        fprintf(stderr, "Soy el cliente. Enviamos un %i.\n",
-                                *cmd);
-                        requests.push(cmd);
-                        break;
-
-                    case SDLK_a:
-                        cmd = new int(2);
-                        fprintf(stderr, "Soy el cliente. Enviamos un %i.\n",
-                                *cmd);
-                        requests.push(cmd);
-                        break;
-
-                    case SDLK_d:
-                        cmd = new int(3);
-                        fprintf(stderr, "Soy el cliente. Enviamos un %i.\n",
-                                *cmd);
-                        requests.push(cmd);
-                        break;
-                    case SDLK_F11:
-                        g_window.fullscreenModeSwitch();
-                        break;
+            switch (event) {
+                case INVALID: {
+                    break;
                 }
-            }
 
-            if (e.type == SDL_KEYUP && e.key.repeat == 0) {
-                switch (e.key.keysym.sym) {
-                    case SDLK_w:
-                    case SDLK_s:
-                    case SDLK_a:
-                    case SDLK_d:
-                        cmd = new int(4);
-                        fprintf(stderr, "Soy el cliente. Enviamos un %i.\n",
-                                *cmd);
-                        requests.push(cmd);
-                        break;
+                case EXIT: {
+                    view_running = false;
+                    break;
+                }
+
+                case START_MOVING_UP: {
+                    /* proxy */
+                    cmd = new int(0);
+                    fprintf(
+                        stderr,
+                        "EVENT-HANDLER | START_MOVING_UP | Enviamos un %i.\n",
+                        *cmd);
+                    requests.push(cmd);
+
+                    break;
+                }
+
+                case START_MOVING_DOWN: {
+                    /* proxy */
+                    cmd = new int(1);
+                    fprintf(
+                        stderr,
+                        "EVENT-HANDLER | START_MOVING_DOWN | Enviamos un %i.\n",
+                        *cmd);
+                    requests.push(cmd);
+
+                    break;
+                }
+
+                case START_MOVING_LEFT: {
+                    cmd = new int(2);
+                    fprintf(
+                        stderr,
+                        "EVENT-HANDLER | START_MOVING_LEFT | Enviamos un %i.\n",
+                        *cmd);
+                    requests.push(cmd);
+
+                    break;
+                }
+
+                case START_MOVING_RIGHT: {
+                    cmd = new int(3);
+                    fprintf(stderr,
+                            "EVENT-HANDLER | START_MOVING_RIGHT | Enviamos un "
+                            "%i.\n",
+                            *cmd);
+                    requests.push(cmd);
+
+                    break;
+                }
+
+                case STOP_MOVING: {
+                    cmd = new int(4);
+                    fprintf(stderr,
+                            "EVENT-HANDLER | STOP_MOVING | Enviamos un %i.\n",
+                            *cmd);
+                    requests.push(cmd);
+
+                    break;
+                }
+
+                default: {
+                    break;
                 }
             }
         }
