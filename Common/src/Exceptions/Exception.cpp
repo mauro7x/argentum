9be1b32c149102d21b22524a9fceffa64000cfad
Mauro7x <mparafati@fi.uba.ar>
2020-06-16 00:55:50 -0300

    Saco el errno_info por ahora

--- Common/src/Exceptions/Exception.cpp
+++ Common/src/Exceptions/Exception.cpp
@@ -4,17 +4,19 @@
 // API Pública
 
 Exception::Exception(const char* fmt, ...) noexcept {
-    int _errno = errno;
+    // int _errno = errno;
     va_list args;
     va_start(args, fmt);
-    int s = vsnprintf(msg_error, BUF_LEN, fmt, args);
+    // int s = vsnprintf(msg_error, BUF_LEN, fmt, args);
+    vsnprintf(msg_error, BUF_LEN, fmt, args);
     va_end(args);
 
-    const char errno_format[] = "\nERRNO_INFO: ";
-    int f = strlen(errno_format);
+    // const char errno_format[] = "\nERRNO_INFO: ";
+    // int f = strlen(errno_format);
 
-    strncpy(msg_error + s, errno_format, BUF_LEN - s);
-    strncpy(msg_error + s + f, strerror(_errno), BUF_LEN - s - f);
+    // strncpy(msg_error + s, errno_format, BUF_LEN - s);
+    // strncpy(msg_error + s + f, strerror(_errno), BUF_LEN - s - f);
+
     msg_error[BUF_LEN - 1] = 0;
 }
 
