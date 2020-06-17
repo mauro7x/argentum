74e66815c225ea48cdcab109d4f73657e26a6ce4
Mauro7x <mparafati@fi.uba.ar>
2020-06-17 05:01:22 -0300

    headers de las clases nuevas

---
+++ Client/includes/Model/CommandDispatcher.h
@@ -0,0 +1,59 @@
+#ifndef __COMMAND_DISPATCHER_H__
+#define __COMMAND_DISPATCHER_H__
+
+//-----------------------------------------------------------------------------
+#include <exception>
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+#include "../../../Common/includes/BlockingQueue.h"
+#include "../../../Common/includes/Socket/SocketWrapper.h"
+#include "../../../Common/includes/Thread.h"
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+#include "Command.h"
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+
+class CommandDispatcher : public Thread {
+   private:
+    const SocketWrapper& socket;       /* SÓLO ESCRITURA (SEND) */
+    BlockingQueue<Command*>& commands; /* Comandos a enviar */
+
+    //-----------------------------------------------------------------------------
+    // Métodos privados
+    //-----------------------------------------------------------------------------
+
+   public:
+    /* Constructor */
+    CommandDispatcher(const SocketWrapper& socket,
+                      BlockingQueue<Command*>& commands);
+
+    /* Deshabilitamos el constructor por copia. */
+    CommandDispatcher(const CommandDispatcher&) = delete;
+
+    /* Deshabilitamos el operador= para copia.*/
+    CommandDispatcher& operator=(const CommandDispatcher&) = delete;
+
+    /* Deshabilitamos el constructor por movimiento. */
+    CommandDispatcher(CommandDispatcher&& other) = delete;
+
+    /* Deshabilitamos el operador= para movimiento. */
+    CommandDispatcher& operator=(CommandDispatcher&& other) = delete;
+
+    //-----------------------------------------------------------------------------
+
+    /* Hilo principal de ejecución */
+    void run() override;
+
+    //-----------------------------------------------------------------------------
+
+    /* Destructor */
+    ~CommandDispatcher();
+};
+
+//-----------------------------------------------------------------------------
+
+#endif  // __COMMAND_DISPATCHER_H__
---
+++ Client/includes/Model/Updater.h
@@ -0,0 +1,59 @@
+#ifndef __UPDATER_H__
+#define __UPDATER_H__
+
+//-----------------------------------------------------------------------------
+#include <exception>
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+#include "../../../Common/includes/Exceptions/Exception.h"
+#include "../../../Common/includes/NonBlockingQueue.h"
+#include "../../../Common/includes/Socket/SocketWrapper.h"
+#include "../../../Common/includes/Thread.h"
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+#include "Update.h"
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+
+class Updater : public Thread {
+   private:
+    const SocketWrapper& socket;        /* SÓLO LECTURA (RECV) */
+    NonBlockingQueue<Update*>& updates; /* Updates a recibir */
+
+    //-----------------------------------------------------------------------------
+    // Métodos privados
+    //-----------------------------------------------------------------------------
+
+   public:
+    /* Constructor */
+    Updater(const SocketWrapper& socket, NonBlockingQueue<Update*>& updates);
+
+    /* Deshabilitamos el constructor por copia. */
+    Updater(const Updater&) = delete;
+
+    /* Deshabilitamos el operador= para copia.*/
+    Updater& operator=(const Updater&) = delete;
+
+    /* Deshabilitamos el constructor por movimiento. */
+    Updater(Updater&& other) = delete;
+
+    /* Deshabilitamos el operador= para movimiento. */
+    Updater& operator=(Updater&& other) = delete;
+
+    //-----------------------------------------------------------------------------
+
+    /* Hilo principal de ejecución */
+    void run() override;
+
+    //-----------------------------------------------------------------------------
+
+    /* Destructor */
+    ~Updater();
+};
+
+//-----------------------------------------------------------------------------
+
+#endif  // __UPDATER_H__
