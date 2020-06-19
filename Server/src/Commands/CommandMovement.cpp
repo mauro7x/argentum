3920e0640ec6e16895520723ae777cb9af5080aa
tony <yhuang@fi.uba.ar>
2020-06-19 14:45:48 -0300

    Modifique los de commands

--- Server/src/Commands/CommandMovement.cpp
+++ Server/src/Commands/CommandMovement.cpp
@@ -8,31 +8,31 @@ CommandMovement::CommandMovement(InstanceId caller, char cmd,
     : caller(caller), cmd(cmd), notifications(notifications) {}
 
 void CommandMovement::operator()(Game& game) {
-    NotificationReply reply;
+    NotificationReply* reply = NULL;
     switch (cmd) {
         case START_MOVING_UP_CMD: {
-            reply = std::move(game.startMovingUp(caller));
-            notifications.push(&reply);
+            reply =game.startMovingUp(caller);
+            notifications.push(reply);
             break;
         }
         case START_MOVING_DOWN_CMD: {
-            reply = std::move(game.startMovingDown(caller));
-            notifications.push(&reply);
+            reply = game.startMovingDown(caller);
+            notifications.push(reply);
             break;
         }
         case START_MOVING_LEFT_CMD: {
-            reply = std::move(game.startMovingLeft(caller));
-            notifications.push(&reply);
+            reply = game.startMovingLeft(caller);
+            notifications.push(reply);
             break;
         }
         case START_MOVING_RIGHT_CMD: {
-            reply = std::move(game.startMovingRight(caller));
-            notifications.push(&reply);
+            reply = game.startMovingRight(caller);
+            notifications.push(reply);
             break;
         }
         case STOP_MOVING_CMD: {
-            reply = std::move(game.stopMoving(caller));
-            notifications.push(&reply);
+            reply = game.stopMoving(caller);
+            notifications.push(reply);
             break;
         }
         default:
