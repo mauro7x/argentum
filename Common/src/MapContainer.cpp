394469637359d0b3d6b3e38f21bed18faf9da394
Mauro7x <mparafati@fi.uba.ar>
2020-06-20 01:56:16 -0300

    corregimos bug de offbyone

--- Common/src/MapContainer.cpp
+++ Common/src/MapContainer.cpp
@@ -49,14 +49,14 @@ const Map& MapContainer::operator[](const Id id) const {
 }
 
 const Id MapContainer::getCharacterSpawningMap() const {
-    std::vector<Id> possible_maps; 
+    std::vector<Id> possible_maps;
     std::unordered_map<Id, Map>::const_iterator it = this->content.begin();
     while (it != this->content.end()) {
         possible_maps.push_back(it->first);
         ++it;
     }
     RandomNumberGenerator gen;
-    return possible_maps.at(gen(0, possible_maps.size()));
+    return possible_maps.at(gen(0, possible_maps.size() - 1));
 }
 
 MapContainer::~MapContainer() {}
