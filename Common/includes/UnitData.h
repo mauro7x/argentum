db4c658ab92088d79e4547804412ef18f3fcd235
Mauro7x <mparafati@fi.uba.ar>
2020-06-20 00:06:44 -0300

    Refactor de unitdata

--- Common/includes/UnitData.h
+++ Common/includes/UnitData.h
@@ -1,30 +1,12 @@
 #ifndef __UNIT_DATA_H__
 #define __UNIT_DATA_H__
+
 //-----------------------------------------------------------------------------
-#include <array>
-//-----------------------------------------------------------------------------
+#include "Inventory.h"
 #include "Orientation.h"
-#include "game_config.h"
 #include "types.h"
 //-----------------------------------------------------------------------------
 
-#ifndef N_WEARABLE_ITEMS
-#define N_WEARABLE_ITEMS 4
-#endif  // N_WEARABLE_ITEMS
-
-//-----------------------------------------------------------------------------
-// ESTRUCTURAS AUXILIARES
-//-----------------------------------------------------------------------------
-
-struct InventorySlot {
-    Id item;
-    uint32_t amount;
-};
-
-//-----------------------------------------------------------------------------
-
-//-----------------------------------------------------------------------------
-// ESTRUCTURA BASE
 //-----------------------------------------------------------------------------
 
 /* Información básica necesaria para cualquier unidad */
@@ -38,42 +20,26 @@ struct UnitData {
 //-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
-// ESTRUCTURAS PARTICULARES
-//-----------------------------------------------------------------------------
-/* Estructura particular para el player principal */
-struct PlayerData {
-    // Data común a todas las unidades
-    UnitData basic_data;
 
-    // Stats y atributos
-    uint32_t health, mana, gold; /* stats */
-
-    // Inventario y equipameniento
-    std::array<InventorySlot, N_INVENTORY_SLOTS> inventory;
-    std::array<Id, N_WEARABLE_ITEMS> equipment;
-
-    // Cuerpo y vestimenta
-    Id head_id, body_id;                           /* cuerpo básico */
-    Id helmet_id, armour_id, shield_id, weapon_id; /* vestimenta */
-};
-
-/* Estructura particular para los players controlados por terceros */
 struct CharacterData {
-    /* Data común a las unidades */
     UnitData basic_data;
-
-    // Cuerpo y vestimenta
-    Id head_id, body_id;                           /* cuerpo básico */
-    Id helmet_id, armour_id, shield_id, weapon_id; /* vestimenta */
+    Id head_id, body_id;
+    Id helmet_id, armour_id, shield_id, weapon_id;
 };
 
-/* Estructura particular para los monstruos */
-struct CreatureData {
-    /* Data común a las unidades */
-    UnitData basic_data;
+struct PlayerData : public CharacterData {
+    uint32_t health, max_health;
+    uint32_t mana, max_mana;
+    uint32_t safe_gold, excess_gold;
+    uint32_t level, exp, levelup_exp;
 
-    // Cuerpo
-    Id creature_id; /* creature body */
+    Inventory inventory;
+    Equipment equipment;
+};
+
+struct CreatureData {
+    UnitData basic_data;
+    Id creature_id;
 };
 
 //-----------------------------------------------------------------------------
--- Common/includes/game_config.h
+++
@@ -1,6 +0,0 @@
-#ifndef __GAME_CONFIG_H__
-#define __GAME_CONFIG_H__
-
-#define N_INVENTORY_SLOTS 10
-
-#endif // __GAME_CONFIG_H___
