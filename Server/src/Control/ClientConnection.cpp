d7d51ba094d89998e1b1c79dabb118a31440a9ad
sankle <santi.klein@hotmail.com>
2020-06-22 17:36:31 -0300

    Agrego condicion de corte extra en sender

--- Server/src/Control/ClientConnection.cpp
+++ Server/src/Control/ClientConnection.cpp
@@ -27,7 +27,7 @@ void ClientConnection::_sender() {
     try {
         Notification* notification = nullptr;
         bool socket_valid = true;
-        while ((notification = notifications.pop())) {
+        while ((notification = notifications.pop()) && !finished_threads) {
             if (notification->isBroadcast()) {
                 if (!(notification->getMapId() == this->map)) {
                     delete notification;
