6c1179940bd01bab0cd619e190623737593556a4
tony <yhuang@fi.uba.ar>
2020-06-22 18:38:10 -0300

    defini los metodos de creatura

---
+++ Server/includes/Model/Creature.h
@@ -0,0 +1,79 @@
+#ifndef __CREATURE_H__
+#define __CREATURE_H__
+
+//-----------------------------------------------------------------------------
+#include "../../../Common/includes/MapContainer.h"
+#include "../../../Common/includes/Orientation.h"
+#include "../../../Common/includes/types.h"
+//-----------------------------------------------------------------------------
+#include "config_structs.h"
+//-----------------------------------------------------------------------------
+#include "Position.h"
+//-----------------------------------------------------------------------------
+
+class Creature {
+   private:
+    unsigned int health_max;
+    unsigned int health_actual;
+    StateType state;
+    Position position;
+
+    Orientation moving_orientation;
+    bool moving;
+    unsigned int moving_time_elapsed;
+    unsigned int attribute_update_time_elapsed;
+
+   public:
+    //-----------------------------------------------------------------------------
+
+    /* Constructor */
+    Creature(unsigned int health, MapContainer& map_container, const Id init_map,
+             const int init_x_coord, const int init_y_coord);
+
+    /* Destructor */
+    ~Creature();
+
+    /* Deshabilitamos el constructor por copia. */
+    Creature(const Creature&) = delete;
+
+    /* Deshabilitamos el operador= para copia.*/
+    Creature& operator=(const Creature&) = delete;
+
+    /* Deshabilitamos el constructor por movimiento. */
+    Creature(Creature&&) = delete;
+
+    /* Habilitamos el operador= para movimiento. */
+    Creature& operator=(Creature&&) = delete;
+
+    /*
+     * Recibe la cantidad de iteraciones que ocurrieron desde la última
+     * vez que se llamó.
+     *
+     * Actualiza los atributos de la creatura que se ven afectados por el
+     * paso del tiempo:
+     *
+     * - posición [si la creatura está en movimiento]
+     */
+    void act(const unsigned int it);
+
+    //-----------------------------------------------------------------------------
+    //  Movimiento
+    //-----------------------------------------------------------------------------
+
+    void startMovingUp();
+    void startMovingDown();
+    void startMovingRight();
+    void startMovingLeft();
+
+    void stopMoving();
+
+    /*
+     * Efectúa la recepción del ataque del jugador.
+     * si el damage supera la vida actual, 
+     */
+    void receiveAttack(const unsigned int damage);
+
+    unsigned int getMaxHealth();
+    
+};
+#endif  //__CREATURE_H__
