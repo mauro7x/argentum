589449d53dad9c6d69699a20640a3b0317e59aa5
tony <yhuang@fi.uba.ar>
2020-06-26 14:44:47 -0300

    defini atributos para persistencia

--- Server/includes/Control/Database.h
+++ Server/includes/Control/Database.h
@@ -5,28 +5,45 @@
 #include <mutex>
 #include <string>
 #include <unordered_map>
+#include <cstring>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <cstring>
+
 //-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
 #include "../../../Common/includes/Exceptions/Exception.h"
 #include "../../../Common/includes/Exceptions/LoginException.h"
 #include "../Model/config_structs.h"
+#include "../paths.h"
 //-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
-struct Userinfo {
-    std::string password;
-    CharacterCfg character_data;
+
+struct PlayerInfo{
+    char username[8] = {0};
+    char password[8] = {0};
+    size_t position;
 };
+
 
 class Database {
    private:
     bool initialized;
+    size_t file_pointer;
     std::mutex m;
-    std::unordered_map<std::string, Userinfo> clients; /* user: pass */
+    std::unordered_map<std::string, CharacterCfg> clients; /* user: data */
+    std::unordered_map<std::string, size_t> data_index; /*user:data_index */
+    std::unordered_map<std::string, std::string> player_infos; /*user:pass*/
 
+    std::fstream file_info;
+    std::fstream file_data;
     //-------------------------------------------------------------------------
     // Métodos privados
+    void _fillInfo (std::fstream& file);
     //-------------------------------------------------------------------------
 
    public:
