aac40d46bfa5ac9e6a59ba0c454531b8c53c9649
tony <yhuang@fi.uba.ar>
2020-06-28 21:41:21 -0300

    saque atributo head_id y body_id en la clase race

--- Server/includes/Model/Race.h
+++ Server/includes/Model/Race.h
@@ -3,33 +3,28 @@
 
 #include <string>
 
-#include "config_structs.h"
-
 #include "../../../Common/includes/types.h"
+#include "config_structs.h"
 
 /*
  * Raza:
  *      Humanos, elfos, enanos, gnomos.
- * 
+ *
  * No tienen comportamiento alguno, simplemente
  * aportan, por un lado, factores a tener en cuenta en el calculo
  * de las ecuaciones de mana y vida, y por otro lado, modificadores
  * que intervienen en el calculo de atributos.
  */
 struct Race {
-        const Id id;
-        const std::string name;
+    const Id id;
+    const std::string name;
+    const unsigned int max_health_factor;
+    const unsigned int max_mana_factor;
+    const unsigned int health_recovery_factor;
+    const unsigned int mana_recovery_factor;
 
-        const Id head_id;
-        const Id body_id;
-
-        const unsigned int max_health_factor;
-        const unsigned int max_mana_factor;
-        const unsigned int health_recovery_factor;
-        const unsigned int mana_recovery_factor;
-
-        Race(const RaceCfg& data);
-        ~Race();
+    Race(const RaceCfg& data);
+    ~Race();
 };
 
 #endif
--- Server/src/Model/Race.cpp
+++ Server/src/Model/Race.cpp
@@ -1,11 +1,11 @@
 #include "../../includes/Model/Race.h"
 
-Race::Race(const RaceCfg& data): 
-    id(data.id), name(data.name),
-    head_id(data.head_id), body_id(data.body_id),
-    max_health_factor(data.max_health_factor),
-    max_mana_factor(data.max_mana_factor),
-    health_recovery_factor(data.health_recovery_factor),
-    mana_recovery_factor(data.mana_recovery_factor) {}
+Race::Race(const RaceCfg& data)
+    : id(data.id),
+      name(data.name),
+      max_health_factor(data.max_health_factor),
+      max_mana_factor(data.max_mana_factor),
+      health_recovery_factor(data.health_recovery_factor),
+      mana_recovery_factor(data.mana_recovery_factor) {}
 
 Race::~Race() {}
