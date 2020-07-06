9a7a2e5ea77696aa314a4249ef60ed28faeb463b
Mauro7x <mparafati@fi.uba.ar>
2020-07-06 00:35:55 -0300

    agrego para debug

--- Client/src/GameModel/Broadcasts/NewCreatureBroadcast.cpp
+++ Client/src/GameModel/Broadcasts/NewCreatureBroadcast.cpp
@@ -15,6 +15,7 @@ void NewCreatureBroadcast::exec(
     MapView& map, Player& player,
     UnitContainer<Character, CharacterData>& characters,
     UnitContainer<Creature, CreatureData>& creatures) {
+    fprintf(stderr, "New creature | id: [%u]\n", data.basic_data.gid);
     creatures.add(data.basic_data.gid, data);
     map.occupyTile(data.basic_data.gid, data.basic_data.x_tile,
                    data.basic_data.y_tile);
